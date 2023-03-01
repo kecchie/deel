0651b9d6927d46122e99f5011a4de2042065721b
Isin Pesch <isinsur06@gmail.com>
2023-03-01 13:16:49 +0300

    created tables

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask
 import psycopg2
 import json
 import datetime
+from psycopg2.extras import execute_values
 
 
 app = Flask(__name__)
@@ -24,88 +25,81 @@ def hello():
 
 if __name__ == '__main__':
 
-    # Open the JSON file and parse its contents as a list of dictionaries
-    # Create the contracts table
-    print('1')
+    # Create a cursor
     cur = conn.cursor()
-    cur.execute("""
-    CREATE TABLE testing (
-        contract_id TEXT PRIMARY KEY,
-        client_id TEXT
-    );
-    """)
 
-    # cur.execute("""
-    # CREATE TABLE IF NOT EXISTS testing (
-    #     contract_id TEXT PRIMARY KEY,
-    #     client_id TEXT,
-    #     contract_created_at TIMESTAMP,
-    #     status TEXT,
-    #     completion_date TIMESTAMP,
-    #     is_deleted BOOLEAN,
-    #     received_at TIMESTAMP
-    # );
-    # """)
+    # Define the contract table schema
+    contract_table = """CREATE TABLE IF NOT EXISTS contract (
+                        contract_id VARCHAR(32) PRIMARY KEY,
+                        client_id VARCHAR(32),
+                        contract_created_at TIMESTAMP,
+                        status VARCHAR(16),
+                        completion_date TEXT,
+                        is_deleted BOOLEAN,
+                        received_at TIMESTAMP,
+                        start_date TIMESTAMP,
+                        end_date TIMESTAMP,
+                        current_flag BOOLEAN
+                        );"""
 
-    print('2')
-    # # Create the invoices table
-    # cur.execute("""
-    # CREATE TABLE IF NOT EXISTS invoices (
-    #     invoice_id TEXT PRIMARY KEY,
-    #     contract_id TEXT REFERENCES contracts (contract_id),
-    #     amount NUMERIC(10,2),
-    #     currency TEXT,
-    #     is_early_paid BOOLEAN,
-    #     is_deleted BOOLEAN,
-    #     received_at TIMESTAMP
-    # )
-    # """)
+    # Define the invoice table schema
+    invoice_table = """CREATE TABLE IF NOT EXISTS invoice (
+                        invoice_id VARCHAR(32) PRIMARY KEY,
+                        contract_id VARCHAR(32),
+                        amount FLOAT,
+                        currency VARCHAR(16),
+                        is_early_paid BOOLEAN,
+                        is_deleted BOOLEAN,
+                        received_at TIMESTAMP,
+                        start_date TIMESTAMP,
+                        end_date TIMESTAMP,
+                        current_flag BOOLEAN
+                        );"""
 
-    # # Load the contracts from the JSON file and insert them into the contracts table
-    # with open('contracts.json', 'r') as f:
-    #     contracts_data = json.load(f)
-    #     contracts = [(c['CONTRACT_ID'], c['CLIENT_ID'], c['CONTRACT_CREATED_AT'], c['STATUS'],
-    #                   c['COMPLETION_DATE'], c['IS_DELETED'], c['RECEIVED_AT']) for c in contracts_data]
-    # print('3')
-    # batch_size = 1000
-    # for i in range(0, len(contracts), batch_size):
-    #     cur.executemany("""
-    #         INSERT INTO contracts (contract_id, client_id, contract_created_at, status, completion_date, is_deleted, received_at)
-    #         VALUES (%s, %s, %s, %s, %s, %s, %s)
-    #         ON CONFLICT (contract_id) DO UPDATE SET
-    #             client_id = excluded.client_id,
-    #             contract_created_at = excluded.contract_created_at,
-    #             status = excluded.status,
-    #             completion_date = excluded.completion_date,
-    #             is_deleted = excluded.is_deleted,
-    #             received_at = excluded.received_at
-    #     """, contracts[i:i+batch_size])
-    #     conn.commit()
-    # print('4')
-    # # Load the invoices from the JSON file and insert them into the invoices table
-    # with open('invoices.json', 'r') as f:
-    #     invoices_data = json.load(f)
-    #     invoices = [(i['INVOICE_ID'], i['CONTRACT_ID'], i['AMOUNT'], i['CURRENCY'],
-    #                 i['IS_EARLY_PAID'], i['IS_DELETED'], i['RECEIVED_AT']) for i in invoices_data]
-    # print('5')
-    # for i in range(0, len(invoices), batch_size):
-    #     cur.executemany("""
-    #         INSERT INTO invoices (invoice_id, contract_id, amount, currency, is_early_paid, is_deleted, received_at)
-    #         VALUES (%s, %s, %s, %s, %s, %s, %s)
-    #         ON CONFLICT (invoice_id) DO UPDATE SET
-    #             contract_id = excluded.contract_id,
-    #             amount = excluded.amount,
-    #             currency = excluded.currency,
-    #             is_early_paid = excluded.is_early_paid,
-    #             is_deleted = excluded.is_deleted,
-    #             received_at = excluded.received_at
-    #     """, invoices[i:i+batch_size])
-    #     conn.commit()
-    #     print('6')
+    # Create the contract table
+    cur.execute(contract_table)
 
-    # # Close the database connection
-    # cur.close()
-    # conn.close()
-    # print('7')
+    # Create the invoice table
+    cur.execute(invoice_table)
 
-    # #app.run(debug=True, host='0.0.0.0')
+    # Commit the changes
+    conn.commit()
+
+    # Open the contracts.json file and load the JSON data
+    with open('contracts.json', 'r') as contracts_file:
+        contracts_data = json.load(contracts_file)
+
+    # Open the invoices.json file and load the JSON data
+    with open('invoices.json', 'r') as invoices_file:
+        invoices_data = json.load(invoices_file)
+
+    # Define the batch size
+    batch_size = 1000
+
+    # Define the start and end dates for the contracts and invoices
+    start_date = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')
+    end_date = '9999-12-31 23:59:59.999999'
+
+    # Load the contracts into the database
+    for i in range(0, len(contracts_data), batch_size):
+        contracts_batch = contracts_data[i:i + batch_size]
+
+        # Create a list of tuples containing the contract data
+        contract_tuples = [(c['CONTRACT_ID'], c['CLIENT_ID'], c['CONTRACT_CREATED_AT'], c['STATUS'], c['COMPLETION_DATE'],
+                            c['IS_DELETED'], c['RECEIVED_AT'], start_date, end_date, True) for c in contracts_batch]
+
+        # Execute the SQL query to insert the contracts
+        execute_values(cur, "INSERT INTO contract (contract_id, client_id, contract_created_at, status, completion_date, is_deleted, received_at, start_date, end_date, current_flag) VALUES %s ON CONFLICT (contract_id) DO UPDATE SET current_flag = FALSE, end_date = excluded.start_date - INTERVAL '1 microsecond'", contract_tuples)
+
+        # Commit the changes
+        conn.commit()
+
+    # Define the start and end dates for the invoices
+    start_date = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')
+
+    # Load the invoices into the database
+    for i in range(0, len(invoices_data), batch_size):
+        invoices_batch = invoices_data[i:i + batch_size]
+        conn.commit()
+
+    app.run(debug=True, host='0.0.0.0')
